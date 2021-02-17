130824ab9ea38502009945e677b2f70e73d02b06
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2021-02-17 13:48:28 +0100

    fix: add test for files with old data structure

--- tests/test_file.py
+++ tests/test_file.py
@@ -14,6 +14,10 @@ def test_file_has_one_repetition():
     num_repetitions = bf.repetition_count()
     assert num_repetitions == 1
 
+    bf = BrillouinFile(data_file_path('Water_old.h5'))
+    num_repetitions = bf.repetition_count()
+    assert num_repetitions == 1
+
 
 def test_non_existing_file_raises_exception():
     with pytest.raises(OSError):
@@ -34,6 +38,9 @@ def test_file_get_repetition_keys():
     bf = BrillouinFile(data_file_path('Water.h5'))
     assert bf.repetition_keys() == ['0']
 
+    bf = BrillouinFile(data_file_path('Water_old.h5'))
+    assert bf.repetition_keys() == ['0']
+
 
 def test_file_get_repetition_date():
     bf = BrillouinFile(data_file_path('Water.h5'))
@@ -52,14 +59,36 @@ def test_file_repetition_has_calibration():
     rep = bf.get_repetition('0')
     assert not rep.calibration.is_empty()
 
+    bf = BrillouinFile(data_file_path('Water_old.h5'))
+    rep = bf.get_repetition('0')
+    assert not rep.calibration.is_empty()
+
 
 def test_file_payload_image_keys():
     bf = BrillouinFile(data_file_path('Water.h5'))
     rep = bf.get_repetition('0')
     assert rep.payload.image_keys() == [str(k) for k in range(10)]
 
+    bf = BrillouinFile(data_file_path('Water_old.h5'))
+    rep = bf.get_repetition('0')
+    assert rep.payload.image_keys() == [str(k) for k in range(4)]
+
 
 def test_file_payload_get_image():
     bf = BrillouinFile(data_file_path('Water.h5'))
     image = bf.get_repetition('0').payload.get_image('0')
     assert image.shape == (2, 400, 400)
+
+    bf = BrillouinFile(data_file_path('Water_old.h5'))
+    image = bf.get_repetition('0').payload.get_image('0')
+    assert image.shape == (2, 400, 400)
+
+
+def test_file_calibration_image_keys():
+    bf = BrillouinFile(data_file_path('Water.h5'))
+    rep = bf.get_repetition('0')
+    assert rep.calibration.image_keys() == [str(k+1) for k in range(2)]
+
+    bf = BrillouinFile(data_file_path('Water_old.h5'))
+    rep = bf.get_repetition('0')
+    assert rep.calibration.image_keys() == [str(k+1) for k in range(2)]
