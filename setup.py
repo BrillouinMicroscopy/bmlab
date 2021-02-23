3017e3a7d4f695a4e7fea568122033e2a749373c
Matthias Baer <matthias.baer@mpl.mpg.de>
2021-02-23 09:49:04 +0100

    Add shapely dependency

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup(
                       "numpy>=1.17.0",
                       "scipy>=0.14.0",
                       "packaging>=20.8",
-                      "scikit_image"
+                      "scikit_image",
+                      "shapely"
                       ],
     # not to be confused with definitions in pyproject.toml [build-system]
     setup_requires=["pytest-runner"],
