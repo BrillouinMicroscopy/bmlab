24b4889362f71df730da905ecfd8419207bf3961
Timon <timon.beck@mpl.mpg.de>
2021-02-23 10:39:44 +0100

    Refined test for fit_circle

--- tests/test_image.py
+++ tests/test_image.py
@@ -42,16 +42,27 @@ def test_circle_fit():
     expect_c = (-200, -220)
     n_test_data_points = 6
     noise_strength = 8
+    np.random.seed(1)
 
     x_noise = np.random.random(n_test_data_points) * noise_strength
     y_noise = np.random.random(n_test_data_points) * noise_strength
-    test_points = [(expect_r * np.cos(phi) + x_noise[i] + expect_c[0],
-                    expect_r * np.sin(phi) + y_noise[i] + expect_c[1])
-                   for i, phi in enumerate(
+
+    test_points_noise = [(expect_r * np.cos(phi) + x_noise[i] + expect_c[0],
+                          expect_r * np.sin(phi) + y_noise[i] + expect_c[1])
+                         for i, phi in enumerate(
             np.linspace(0, np.pi / 2, n_test_data_points))
+                         ]
+
+    actual_c_noise, actual_r_noise = fit_circle(test_points_noise)
+    np.testing.assert_allclose(actual_c_noise, expect_c, rtol=0.1)
+    np.testing.assert_allclose(actual_r_noise, expect_r, rtol=0.1)
+
+    test_points = [(expect_r * np.cos(phi) + expect_c[0],
+                    expect_r * np.sin(phi) + expect_c[1])
+                   for i, phi in enumerate(
+            np.linspace(0, np.pi / 2, int(n_test_data_points / 2)))
                    ]
+
     actual_c, actual_r = fit_circle(test_points)
-
-    np.testing.assert_allclose(actual_c, expect_c, rtol=0.1)
-
-    np.testing.assert_allclose(actual_r, expect_r, rtol=0.1)
+    np.testing.assert_allclose(actual_c, expect_c, rtol=1e-10)
+    np.testing.assert_allclose(actual_r, expect_r, rtol=1e-10)
