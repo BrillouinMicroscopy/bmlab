fix: use average instead of sum for spectrum extraction

bmlab/image.py
@@ -148,7 +148,7 @@ def extract_lines_along_arc(img, orientation, phis, circle, num_points):
         points = [mid_point + e_r *
                   k for k in np.arange(-num_points, num_points+1)]
         points = np.array(points)
-        values.append(sum(interpolate(img, p) for p in points))
+        values.append(sum(interpolate(img, p) for p in points) / (points.size / 2))
     return np.array(values)
 
 

tests/test_image.py
@@ -98,9 +98,8 @@ def test_extract_lines_along_arc():
 
     # Assert
     one = np.ones_like(phis)
-    # 7 = 3 + 1 + 3 points, one in the middle, three inside, three outside
-    np.testing.assert_allclose(actual, 7*100*one, rtol=1.E-4)
-    np.testing.assert_allclose(actual_2, 7 * 10**4 * one, rtol=1.E-3)
+    np.testing.assert_allclose(actual, 100*one, rtol=1.E-4)
+    np.testing.assert_allclose(actual_2, 10**4 * one, rtol=1.E-3)
 
     # Quadratic scaling should shift the sum to the outside
     assert np.all(actual_2 > actual)