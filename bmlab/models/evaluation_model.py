1be8685170d3462b4e884539ad5b579b4a0d6270
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2021-04-27 17:05:02 +0200

    fix: move evaluation parameter list into BMicro, allow to select parameter to show

--- bmlab/models/evaluation_model.py
+++ bmlab/models/evaluation_model.py
@@ -13,49 +13,24 @@ class EvaluationModel(object):
 
         self.results = {
             # Fitted values
-            'brillouin_peak_position': [],   # [pix] Brillouin peak position
-            'brillouin_peak_fwhm': [],       # [pix] Brillouin peak FWHM
+            'brillouin_peak_position': [],   # [pix]  Brillouin peak position
+            'brillouin_peak_fwhm': [],       # [pix]  Brillouin peak FWHM
             'brillouin_peak_intensity': [],  # [a.u.] Brillouin peak intensity
 
-            'rayleigh_peak_position': [],   # [pix] Rayleigh peak position
-            'rayleigh_peak_fwhm': [],       # [pix] Rayleigh peak FWHM
-            'rayleigh_peak_intensity': [],  # [a.u.] Rayleigh peak intensity
+            'rayleigh_peak_position': [],   # [pix]   Rayleigh peak position
+            'rayleigh_peak_fwhm': [],       # [pix]   Rayleigh peak FWHM
+            'rayleigh_peak_intensity': [],  # [a.u.]  Rayleigh peak intensity
 
-            'intensity': [],                # [a.u.] Overall intensity of image
-            'time': [],                     # [s] The time the measurement
-                                            #     point was taken at
+            'intensity': [],                # [a.u.]  Overall intensity
+            'time': [],                     # [s]     The time the measurement
+                                            #         point was taken at
 
             # Derived values
-            'brillouin_shift': [],          # [pix] Brillouin frequency shift
-            'brillouin_shift_f': [],        # [GHz] Brillouin frequency shift
-            'brillouin_peak_fwhm_f': [],    # [GHz] Brillouin peak FWHM
+            'brillouin_shift': [],          # [pix]   Brillouin frequency shift
+            'brillouin_shift_f': [],        # [GHz]   Brillouin frequency shift
+            'brillouin_peak_fwhm_f': [],    # [GHz]   Brillouin peak FWHM
+            'rayleigh_peak_fwhm_f': [],     # [GHz]   Rayleigh peak FWHM
         }
-
-        self.parameters = {
-            'brillouin_shift_p': {      # Brillouin frequency shift
-                'unit', 'pix',
-                'symbol', r'$\nu_\mathrm{B}$',
-                'label', 'Brillouin frequency shift'
-            },
-            'brillouin_shift_f': {      # Brillouin frequency shift
-                'unit', 'GHz',
-                'symbol', r'$\nu_\mathrm{B}$',
-                'label', 'Brillouin frequency shift'
-            },
-            'brillouin_fwhm_p': {       # Brillouin peak width
-                'unit', 'pix',
-                'symbol', r'$\Delta_\mathrm{B}$',
-                'label', 'Brillouin peak width'
-            },
-            'brillouin_fwhm_f': {       # Brillouin peak width
-                'unit', 'GHz',
-                'symbol', r'$\Delta_\mathrm{B}$',
-                'label', 'Brillouin peak width'
-            }
-        }
-
-    def get_parameters(self):
-        return self.parameters
 
     def initialize_results_arrays(self, dims):
         shape_general = (
