daf436d9a9d57242d9c214d93e59b396a3139b3a
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2021-04-27 13:49:39 +0200

    fix: flake8

--- bmlab/controllers/evaluation_controller.py
+++ bmlab/controllers/evaluation_controller.py
@@ -80,11 +80,12 @@ class EvaluationController(object):
                         if max_count is not None:
                             max_count.value = -1
                         return
-                    spectra, times, intensities = self.session.extract_payload_spectrum(
-                        image_key
-                    )
-                    evm.results['time'][ind_x, ind_y, ind_z, :] = times
-                    evm.results['intensity'][ind_x, ind_y, ind_z, :] = intensities
+                    spectra, times, intensities =\
+                        self.session.extract_payload_spectrum(image_key)
+                    evm.results['time'][ind_x, ind_y, ind_z, :] =\
+                        times
+                    evm.results['intensity'][ind_x, ind_y, ind_z, :] =\
+                        intensities
                     # Loop over all frames per measurement position
                     for frame_num, spectrum in enumerate(spectra):
                         xdata = np.arange(len(spectrum))
