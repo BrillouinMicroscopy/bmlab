f997ce9bd3859eff69dd91a4e9aaf00cce35ee3b
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2021-04-29 13:44:05 +0200

    fix: add test for calibration model get_frequency_by_time

--- tests/models/test_calibration_model.py
+++ tests/models/test_calibration_model.py
@@ -1,5 +1,7 @@
 from bmlab.models.calibration_model import CalibrationModel
 
+import numpy as np
+
 
 def test_calibration_model_add_brillouin_region():
     cm = CalibrationModel()
@@ -53,3 +55,26 @@ def test_calibration_model_set_rayleigh_region():
     rayleigh_region_0 = cm.get_rayleigh_regions(0)
 
     assert rayleigh_region_0 == [(1, 3)]
+
+
+def test_get_frequency_by_time():
+    cm = CalibrationModel()
+
+    """
+    Fill calibration model with values for calibrations
+    """
+    cm.set_frequencies('0', 0, np.arange(100).reshape(1, -1))
+    cm.set_frequencies('1', 10, np.arange(100).reshape(1, -1) + 10)
+    cm.set_frequencies('2', 30, np.arange(100).reshape(1, -1) + 30)
+
+    frequency = cm.get_frequency_by_time(0, 0)
+    assert frequency == 0
+
+    frequency = cm.get_frequency_by_time(10, 0)
+    assert frequency == 10
+
+    frequency = cm.get_frequency_by_time(20, 0)
+    assert frequency == 20
+
+    frequency = cm.get_frequency_by_time(20, 11)
+    assert frequency == 31
