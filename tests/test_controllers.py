fe2a4bef1a868e77f2270535fdaf281d93bed7e7
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2022-01-07 17:05:47 +0100

    Work on automatically finding peaks in calibration image

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -44,6 +44,46 @@ def test_optimize_points_in_extraction_model(mocker):
     assert (80, 80) == opt_points[1]
 
 
+def test_distance_point_to_line():
+    ec = ExtractionController()
+
+    np.testing.assert_almost_equal(0, ec.distance_point_to_line(
+        (0.5, 0.5), (1, 1), (0, 0)
+    ))
+    np.testing.assert_almost_equal(0.5, ec.distance_point_to_line(
+        (0.5, 0.5), (0, 0), (0, 1)
+    ))
+    np.testing.assert_almost_equal(np.sqrt(0.5), ec.distance_point_to_line(
+        (0, 1), (1, 1), (0, 0)
+    ))
+
+
+def test_find_points_in_extraction_model(mocker):
+
+    imgs = np.ones((1, 400, 200), dtype=int)
+    imgs[0, 19:22, 19:22] = 5
+    imgs[0, 379:382, 19:22] = 5
+    imgs[0, 19:22, 179:182] = 5
+    imgs[0, 379:382, 179:182] = 5
+
+    em = ExtractionModel()
+
+    mocker.patch('bmlab.session.Session.extraction_model', return_value=em)
+    mocker.patch('bmlab.session.Session.get_calibration_image',
+                 return_value=imgs)
+    mocker.patch('bmlab.session.Session.get_calibration_time',
+                 return_value=0)
+
+    ec = ExtractionController()
+
+    ec.find_points('0', 1, 4, 10)
+    opt_points = em.get_points('0')
+
+    assert len(opt_points) == 2
+    assert (19, 180) == opt_points[0]
+    assert (379, 20) == opt_points[1]
+
+
 def test_calibrate():
 
     calib_key = '0'
--- tests/test_run_bmlab_pipeline.py
+++ tests/test_run_bmlab_pipeline.py
@@ -31,6 +31,7 @@ def run_pipeline():
     })
 
     # Models
+    em = session.extraction_model()
     cm = session.calibration_model()
     pm = session.peak_selection_model()
 
@@ -38,18 +39,34 @@ def run_pipeline():
     cc = CalibrationController()
     evc = EvaluationController()
 
-    points = [
-        (107, 293),
-        (165, 236),
-        (255, 137),
-        (291, 93),
-    ]
+    points = {
+        '1': [
+            (31, 358),
+            (107, 293),
+            (165, 237),
+            (254, 137),
+            (291, 92),
+            (323, 51),
+        ],
+        '2': [
+            (30, 358),
+            (107, 293),
+            (165, 237),
+            (255, 137),
+            (291, 92),
+            (323, 51),
+            (335, 35),
+        ]
+    }
+
     # First add all extraction points because this
     # can influence the extraction for other calibrations
     for calib_key in session.get_calib_keys():
-        for p in points:
-            ec.add_point(calib_key, p)
-        ec.optimize_points(calib_key)
+        ec.find_points(calib_key)
+
+        p = em.get_points(calib_key)
+
+        assert p == points[calib_key]
 
     # Then do the calibration
     for calib_key in session.get_calib_keys():
