4c816bff53f54dded1bfb00e45a32af09dd9404c
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2022-01-21 11:52:31 +0100

    Happy new year!

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ authors = [
 ]
 description = 'Library for Brillouin microscopy data analysis'
 name = 'bmlab'
-year = "2021"
+year = "2022"
 
 
 sys.path.insert(0, realpath(dirname(__file__))+"/"+name)
