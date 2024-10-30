1bd166cf7e1a13341e6cacae5641fcd3491c6bb3
Raimund Schlüßler <rs@cellsense-technologies.com>
2024-10-30 16:00:38 +0100

    feat: add support to handle RGB brightfield images

--- bmlab/export/fluorescence_export.py
+++ bmlab/export/fluorescence_export.py
@@ -52,8 +52,11 @@ class FluorescenceExport(object):
             for image_key in image_keys:
                 channel = repetition.payload.get_channel(image_key)
                 img_data = repetition.payload.get_image(image_key)
-                # Average all images acquired
-                img_data = np.nanmean(img_data, axis=0).astype(np.ubyte)
+
+                # Average all images acquired if grayscale
+                if (repetition.payload.get_class(image_key).casefold()
+                        == 'image_grayscale'):
+                    img_data = np.nanmean(img_data, axis=0).astype(np.ubyte)
 
                 # Construct export path and create it if necessary
                 if self.file.path.parent.name == 'RawData':
@@ -162,8 +165,10 @@ class FluorescenceExport(object):
                     image_warped =\
                         Image.merge("RGB", (blank, blank, image_warped))
                 image_alpha = Image.fromarray(
-                    (255 * np.logical_not(
-                        np.isnan(image_data_warped))).astype(np.ubyte))
+                    np.nanmean(255 * np.logical_not(
+                        np.isnan(image_data_warped)),
+                               axis=tuple(range(2, image_data_warped.ndim)))
+                    .astype(np.ubyte))
                 image_warped.putalpha(image_alpha)
 
                 filename = path / f"{self.file.path.stem}" \
